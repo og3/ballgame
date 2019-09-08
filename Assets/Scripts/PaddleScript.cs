625f0cc6fcd58a01df5b3da64e33d5f00d4e787a
noriyuki togashi <>
2019-09-09 07:28:46 +0900

    #14まで

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PaddleScript : MonoBehaviour
 {
+    //private float speed;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //speed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, 0f) ;
+        Debug.Log(transform.position.x);
     }
 }
+
+
+
+
+
+

3ad987b8293099db1ac8262980ac0b0817a5cf99
noriyuki togashi <>
2019-09-09 08:57:40 +0900

    #17まで

---
+++ Assets/Scripts/BallFactoryScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFactoryScript : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+        InvokeRepeating();
+    }
+
+    void SpawnBall()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(5f, 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0f, 0f, -1 * speed * Time.deltaTime);
+    }
+}
